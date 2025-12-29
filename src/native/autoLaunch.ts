398d64645c145c6713081c9bb14dd81a1e16a114
izzy <me@insrt.uk>
2025-12-29 17:35:24 +0000

    chore: use Stoat name for auto launch entry

--- src/native/autoLaunch.ts
+++ src/native/autoLaunch.ts
@@ -5,7 +5,7 @@ import { ipcMain } from "electron";
 import { mainWindow } from "./window";
 
 export const autoLaunch = new AutoLaunch({
-  name: "Revolt",
+  name: "Stoat",
 });
 
 ipcMain.on("isAutostart?", () =>

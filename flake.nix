24cb54e9647c20b583ba079713a0afdbb4849daa
Amy <amy+git@amogus.cloud>
2026-03-03 15:29:25 -0400

    chore: make nix devshell flake, include pnpm
    Signed-off-by: Amelia <amy+git@amogus.cloud>

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Stoat for Desktop Development shell";
+
+  inputs = {
+    nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.zst";
+  };
+
+  outputs = inputs: {
+    devShells = builtins.mapAttrs (system: pkgs: {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          mise
+          pnpm
+          (writeShellScriptBin "electron-nix" ''
+            exec ${pkgs.lib.getExe pkgs.electron} "$@"
+          '')
+          zip
+          dpkg
+          fakeroot
+          flatpak
+          flatpak-builder
+          elfutils
+        ];
+
+        shellHook = ''
+          export ELECTRON_OVERRIDE_DIST_PATH="${pkgs.electron}/bin"
+          export MISE_NODE_COMPILE=false
+        '';
+      };
+    }) inputs.nixpkgs.legacyPackages;
+  };
+}

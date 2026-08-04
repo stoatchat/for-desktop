eb66e3b9c79c3558eeaa0e06a0fc0961f0932c29
Jacob Schlecht <dadadah@echoha.us>
2026-08-04 00:04:55 -0600

    chore: Remove unneeded dependency
    Signed-off-by: Jacob Schlecht <dadadah@echoha.us>

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
-import nativePlugin from "vite-plugin-native-modules";
 
 // https://vitejs.dev/config
 export default defineConfig({
-  plugins: [nativePlugin()],
   build: {
     commonjsOptions: {
       include: [/node-pipewire/, /node_modules/],
